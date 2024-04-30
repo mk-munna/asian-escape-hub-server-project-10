513d06383ed361fea4008ccd2c2db6747125eda8
mk-munna <mkmunnaofficial@gmail.com>
2024-04-30 23:58:49 +0600

    server site deploy done

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-// console.log(process.env.DB_USER)
-// console.log(process.env.DB_PASS)
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yfvcqxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,7 +38,7 @@ async function run() {
             const touristSpots = await touristSpotCollection.find({}).toArray();
             res.send(touristSpots);
         })
-
+// coutry data
         app.get('/country', async (req, res) => {
             const countries = await CountryCollection.find({}).toArray();
             res.send(countries);
